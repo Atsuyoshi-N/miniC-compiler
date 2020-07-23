4cae88e1d7688a262d706abe356d25212a2a6c1c
Atsuyoshi-N <ash.sit.you7@ezweb.ne.jp>
2020-07-23 19:12:21 +0900

    Add "for" statement

--- parse.c
+++ parse.c
@@ -88,6 +88,7 @@ Node *read_expr_stmt() {
 // stmt = "return" expr ";"
 //      | "if" "(" expr ")" stmt ("else" stmt)?
 //      | "while" "(" expr ")" stmt
+//      | "for" "(" expr? ";" expr? ";" expr? ")" stmt
 //      | expr ";"
 Node *stmt() {
   if (consume("return")) {
@@ -115,6 +116,26 @@ Node *stmt() {
     node->then = stmt();
     return node;
   }
+
+  if (consume("for")) {
+    Node *node = new_node(ND_FOR);
+    expect("(");
+    if(!consume(";")) {
+      node->init = read_expr_stmt();
+      expect(";");
+    }
+    if(!consume(";")) {
+      node->cond = expr();
+      expect(";");
+    }
+    if(!consume(")")) {
+      node->inc = read_expr_stmt();
+      expect(")");
+    }
+    node->then = stmt();
+    return node;
+  }
+
   Node *node = read_expr_stmt();
   expect(";");
   return node;
