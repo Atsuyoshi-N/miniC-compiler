45d706f221f719f1502bf173f11dc98e70a457a8
Atsuyoshi-N <ash.sit.you7@ezweb.ne.jp>
2020-08-07 22:30:46 +0900

    Support function call with up to 6 parameters

--- master/parse.c
+++ master/parse.c
@@ -227,6 +227,21 @@ static Node *mul(void) {
   }
 }
 
+// func-args = "(" (assign ("," assign)*)? ")"
+static Node *func_args(void) {
+  if (consume(")"))
+    return NULL;
+
+  Node *head = assign();
+  Node *cur = head;
+  while(consume(",")) {
+    cur->next = assign();
+    cur = cur->next;
+  }
+  expect(")");
+  return head;
+}
+
 // unary = ("+" | "-")? unary
 //       | primary
 static Node *unary(void) {
@@ -237,8 +252,7 @@ static Node *unary(void) {
   return primary();
 }
 
-// primary = "(" expr ")" | ident args? | num
-// args = "(" ")"
+// primary = "(" expr ")" | ident func-args? | num
 static Node *primary(void) {
   if (consume("(")) {
     Node *node = expr();
@@ -250,9 +264,9 @@ static Node *primary(void) {
   if (tok) {
     // Function call
     if (consume("(")) {
-      expect(")");
       Node *node = new_node(ND_FUNCALL);
       node->funcname = strndup(tok->str, tok->len);
+      node->args = func_args();
       return node;
     }
 
