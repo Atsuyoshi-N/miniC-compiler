3e8c1c637afd2126f5015439089663eff9d72e90
Atsuyoshi-N <ash.sit.you7@ezweb.ne.jp>
2020-08-10 23:47:54 +0900

    Add char type

--- master/parse.c
+++ master/parse.c
@@ -128,10 +128,15 @@ Program *program(void) {
   return prog;
 }
 
-// basetype = "int" "*"*
+// basetype = ('char" | int") "*"*
 static Type *basetype(void) {
-  expect("int");
-  Type *ty = int_type;
+  Type *ty;
+  if (consume("char")) {
+    ty = char_type;
+  } else {
+    expect("int");
+    ty = int_type;
+  }
   while (consume("*"))
     ty = pointer_to(ty);
   return ty;
@@ -230,6 +235,11 @@ static Node *declaration(void) {
   return new_unary(ND_EXPR_STMT, node, tok);
 }
 
+// Returs ture if the next token represents a type.
+static bool is_typename(void) {
+  return (peek("char") || peek("int"));
+}
+
 static Node *stmt(void) {
   Node *node = stmt2();
   add_type(node);
@@ -304,7 +314,7 @@ static Node *stmt2(void) {
     return node;
   }
 
-  if (tok = peek("int"))
+  if (is_typename())
     return declaration();
 
   Node *node = read_expr_stmt();
