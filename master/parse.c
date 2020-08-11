4a4a8f7b14a0db355b38369e28bf658898ce176d
Atsuyoshi-N <ash.sit.you7@ezweb.ne.jp>
2020-08-12 07:27:55 +0900

    Add "->" operator

--- master/parse.c
+++ master/parse.c
@@ -589,7 +589,7 @@ static Node *struct_ref(Node *lhs) {
   return node;
 }
 
-// postfix = primary ("[" expr "]" | "." ident)*
+// postfix = primary ("[" expr "]" | "." ident | "->" ident)*
 static Node *postfix(void) {
   Node *node = primary();
   Token *tok;
@@ -604,6 +604,13 @@ static Node *postfix(void) {
     }
 
     if (tok = consume(".")) {
+      node = struct_ref(node);
+      continue;
+    }
+
+    if (tok = consume("->")) {
+      // x->y is short for (*x).y
+      node = new_unary(ND_DEREF, node, tok);
       node = struct_ref(node);
       continue;
     }
