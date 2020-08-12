9d995f3ad8668850e34474cfe82bc8d4e0146ee5
Atsuyoshi-N <ash.sit.you7@ezweb.ne.jp>
2020-08-12 12:36:16 +0900

    Add function declaration

--- master/parse.c
+++ master/parse.c
@@ -193,11 +193,14 @@ Program *program(void) {
 
   while (!at_eof()) {
     if (is_function()) {
-      cur->next = function();
+      Function *fn = function();
+      if (!fn)
+        continue;
+      cur->next = fn;
       cur = cur->next;
-    } else {
-      global_var();
+      continue;
     }
+    global_var();
   }
 
   Program *prog = calloc(1, sizeof(Program));
@@ -343,7 +346,7 @@ static VarList *read_func_params(void) {
   return head;
 }
 
-// function =  basetype declarator "(" params? ")" "{" stmt* "}"
+// function =  basetype declarator "(" params? ")" ("{" stmt* "}" | ";")
 // params   = param ("," param)*
 // param = basetype declarator type-suffix
 static Function *function(void) {
@@ -359,10 +362,15 @@ static Function *function(void) {
 
   Scope *sc = enter_scope();
   fn->params = read_func_params();
-  expect("{");
+
+  if (consume(";")) {
+    leave_scope(sc);
+    return NULL;
+  }
 
   Node head = {};
   Node *cur = &head;
+  expect("{");
   while (!consume("}")) {
     cur->next = stmt();
     cur = cur->next;
