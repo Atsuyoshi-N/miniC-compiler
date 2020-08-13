905b91a065aecf3b6c7cd62dca45eee40c0788df
Atsuyoshi-N <ash.sit.you7@ezweb.ne.jp>
2020-08-14 06:08:05 +0900

    Add continue statement

--- master/parse.c
+++ master/parse.c
@@ -712,6 +712,7 @@ static Node *stmt(void) {
 //      | "for" "(" (expr? ";" | declaration) expr? ";" expr? ")" stmt
 //      | "{" stmt* "}"
 //      | "break" ";"
+//      | "continue" ";"
 //      | declaration
 //      | expr ";"
 static Node *stmt2(void) {
@@ -788,6 +789,11 @@ static Node *stmt2(void) {
   if (tok = consume("break")) {
     expect(";");
     return new_node(ND_BREAK, tok);
+  }
+
+  if (tok = consume("continue")) {
+    expect(";");
+    return new_node(ND_CONTINUE, tok);
   }
 
   if (is_typename())
