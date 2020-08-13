14f66d8dc13cf940d0e00e878ae2e56a1dac29a3
Atsuyoshi-N <ash.sit.you7@ezweb.ne.jp>
2020-08-13 19:34:21 +0900

    Add ~ operator

--- master/parse.c
+++ master/parse.c
@@ -881,7 +881,7 @@ static Node *cast(void) {
   return unary();
 }
 
-// unary = ("+" | "-" | "*" | "&" | "!")? cast
+// unary = ("+" | "-" | "*" | "&" | "!" | "~")? cast
 //       | ("++" | "--") unary
 //       | postfix
 static Node *unary(void) {
@@ -896,6 +896,8 @@ static Node *unary(void) {
     return new_unary(ND_DEREF, cast(), tok);
   if (tok = consume("!"))
     return new_unary(ND_NOT, cast(), tok);
+  if (tok = consume("~"))
+    return new_unary(ND_BITNOT, cast(), tok);
   if (tok = consume("++"))
     return new_unary(ND_PRE_INC, unary(), tok);
   if (tok = consume("--"))
