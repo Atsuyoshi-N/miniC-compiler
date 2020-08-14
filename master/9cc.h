12e21886dc6a045dd93ff9dfa08dafbdd5fda612
Atsuyoshi-N <ash.sit.you7@ezweb.ne.jp>
2020-08-15 07:59:59 +0900

    Add static global variables

--- master/9cc.h
+++ master/9cc.h
@@ -72,6 +72,7 @@ struct Var {
   int offset;    // Offset from RBP
 
   // Global variable
+  bool is_static;
   Initializer *initializer;
 };
 
--- master/parse.c
+++ master/parse.c
@@ -137,8 +137,9 @@ static Var *new_lvar(char *name, Type *ty) {
   return var;
 }
 
-static Var *new_gvar(char *name, Type *ty, bool emit) {
+static Var *new_gvar(char *name, Type *ty, bool is_static, bool emit) {
   Var *var = new_var(name, ty, false);
+  var->is_static = is_static;
   push_scope(name)->var = var;
 
   if (emit) {
@@ -637,7 +638,7 @@ static Function *function(void) {
   ty = declarator(ty, &name);
 
   // Add a function type to the scope
-  new_gvar(name, func_type(ty), false);
+  new_gvar(name, func_type(ty), false, false);
 
   // Construct a function object
   Function *fn = calloc(1, sizeof(Function));
@@ -856,7 +857,7 @@ static void global_var(void) {
     return;
   }
 
-  Var *var = new_gvar(name, ty, sclass != EXTERN);
+  Var *var = new_gvar(name, ty, sclass == STATIC, sclass != EXTERN);
 
   if (sclass == EXTERN) {
     expect(";");
@@ -1077,7 +1078,7 @@ static Node *declaration(void) {
 
   if (sclass == STATIC) {
     // static local variable
-    Var *var = new_gvar(new_label(), ty, true);
+    Var *var = new_gvar(new_label(), ty, true, true);
     push_scope(name)->var = var;
 
     if (consume("="))
@@ -1699,7 +1700,7 @@ static Node *compound_literal(void) {
   }
 
   if (scope_depth == 0) {
-    Var *var = new_gvar(new_label(), ty, true);
+    Var *var = new_gvar(new_label(), ty, true, true);
     var->initializer = gvar_initializer(ty);
     return new_var_node(var, tok);
   }
@@ -1828,7 +1829,7 @@ static Node *primary(void) {
   if (tok->kind == TK_STR) {
     token = token->next;
     Type *ty = array_of(char_type, tok->cont_len);
-    Var *var = new_gvar(new_label(), ty, true);
+    Var *var = new_gvar(new_label(), ty, true, true);
     var->initializer = gvar_init_string(tok->contents, tok->cont_len);
     return new_var_node(var, tok);
   }
